c5808d05c5ef877a2b964b100f807ca9b890b0ee
Ori Roll <oriroll@gmail.com>
2022-08-12 14:56:43 +0300

    More fixes for data handler WIP

--- src/components/GifAutocomplete/dataHandlers/gifDataHandler.tsx
+++ src/components/GifAutocomplete/dataHandlers/gifDataHandler.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import { MultiResponse } from 'giphy-api';
 import { callSearchGiphyApi } from '../../../apiCalls';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { onGifSearchResponseSuccess } from '../slice/gifAutocompleteSlice';
+import { store } from '../../../stores';
 
-const getSearchData = async (searchPhrase: string) => {
-  const res = await callSearchGiphyApi(searchPhrase);
+const getSearchData = async (
+  searchPhrase: string,
+  searchParams?: Record<string, string | number>
+) => {
+  const res = await callSearchGiphyApi(searchPhrase, searchParams);
   return res as MultiResponse;
 };
 
-const useGifDataHandler = async (searchPhrase: string) => {
-  const dispatch = useDispatch();
+const gifGetDataHandler = async (searchPhrase: string) => {
+  const { data, pagination }: MultiResponse = await getSearchData(
+    searchPhrase,
+    { limit: 9 }
+  );
 
-  const { data, pagination }: MultiResponse = await getSearchData(searchPhrase);
-
-  // const data: CurrentGifsSearch = useSelector(
-  //   (state: GifAutocompleteSliceState) => state.currentGifsSearch
-  // );
-
-  // const newData = dispatch(
+  // const dispatch = useDispatch();
+  // store.dispatch(
   //   onGifSearchResponseSuccess({
-  //     totalCount: pagination.total_count,
-  //     count: pagination.count,
-  //     offset: pagination.offset,
+  //     // totalCount: pagination.total_count,
+  //     // count: pagination.count,
+  //     // offset: pagination.offset,
+  //     // data: [],
+  //     totalCount: null,
+  //     count: null,
+  //     offset: null,
   //     data: [],
   //   })
   // );
+
+  //  dispatch(
+  //     onGifSearchResponseSuccess({
+  //       // totalCount: pagination.total_count,
+  //       // count: pagination.count,
+  //       // offset: pagination.offset,
+  //       // data: [],
+  //       totalCount: null,
+  //       count: null,
+  //       offset: null,
+  //       data: [],
+  //     })
+  //   );
 };
