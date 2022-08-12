e1bd57cefa14a1d2e8b2770a770c9fa1a0f6a4e5
Ori Roll <oriroll@gmail.com>
2022-08-12 14:58:38 +0300

    added searchParams to callSearchGiphyApi

--- src/apiCalls/giphyApi/giphyApiCalls.ts
+++ src/apiCalls/giphyApi/giphyApiCalls.ts
@@ -3,25 +3,30 @@ import { handleError } from '../apiErrorHandlers/basicErrorHandle';
 import { MultiResponse } from 'giphy-api';
 
 import axios from 'axios';
+import { join } from 'path';
 
 const GIPHY_API_KEY = 'BPQxDk7fvxwaFbNgBX9xOdvGpBDgEysB';
 const GIPHY_BASE_URL = 'https://api.giphy.com/v1';
 const GIPHY_GIF_SEARCH_URL = '/gifs/search';
 
 export const callSearchGiphyApi = async (
-  phrase: string
+  phrase: string,
+  searchParams?: Record<string, string | number>
 ): Promise<MultiResponse | undefined> => {
   if (!phrase) console.error('no search phrase provided');
 
-  const fullSearchUrl = `${GIPHY_BASE_URL}${GIPHY_GIF_SEARCH_URL}?api_key=${GIPHY_API_KEY}&q=${phrase}`;
+  const additionalParams = searchParams
+    ? Object.entries(searchParams)
+        .map(([key, value]: any[]) => `${key}${value}`)
+        .join('')
+    : '';
+
+  const fullSearchUrl = `${GIPHY_BASE_URL}${GIPHY_GIF_SEARCH_URL}?api_key=${GIPHY_API_KEY}&q=${phrase}${additionalParams}`;
 
   // TODO: Is this the right way?
   try {
     const { data } = await axios.get<MultiResponse>(fullSearchUrl, {
-      headers: {
-        // api_key: apiKey,
-        // q: 'cheeseburgers',
-      },
+      headers: {},
     });
 
     if (!data) throw new Error('no data');
