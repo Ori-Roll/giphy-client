64b85a3a5bd2e4ca6038844014bef46bfb25faf0
Ori Roll <oriroll@gmail.com>
2022-08-12 11:36:44 +0300

    api call initial test - api search call data - test in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { callSearchGiphyApi } from './apiCalls';
 import './App.css';
 
+const callAPI = async () => {
+  const res = await callSearchGiphyApi('cheeseburgers');
+  console.log(JSON.stringify(res, null, 2));
+};
+
 function App() {
+  useEffect(() => {
+    callAPI();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">GIPHY SEARCH</header>
--- src/apiCalls/giphyApi/giphyApiCalls.ts
+++ src/apiCalls/giphyApi/giphyApiCalls.ts
@@ -4,18 +4,27 @@ import { SingleResponse } from 'giphy-api';
 
 import axios from 'axios';
 
-const apiKey = 'BPQxDk7fvxwaFbNgBX9xOdvGpBDgEysB';
+const GIPHY_API_KEY = 'BPQxDk7fvxwaFbNgBX9xOdvGpBDgEysB';
+const GIPHY_BASE_URL = 'https://api.giphy.com/v1/gifs/search';
+const GIPHY_GIF_SEARCH_URL = '/gifs/search';
 
-const callSearchGiphyApi = async () => {
+export const callSearchGiphyApi = async (phrase: string) => {
+  if (!phrase) console.error('no search phrase provided');
+
+  const fullSearchUrl = `${GIPHY_BASE_URL}${GIPHY_GIF_SEARCH_URL}?api_key=${GIPHY_API_KEY}&q=${phrase}`;
+
   try {
     const response = await axios.get<SingleResponse>(
-      'https://api.giphy.com/v1/gifs/search',
+      'https://api.giphy.com/v1/gifs/search?api_key=BPQxDk7fvxwaFbNgBX9xOdvGpBDgEysB&q=cheeseburgers',
       {
         headers: {
-          api_key: apiKey,
+          // api_key: apiKey,
+          // q: 'cheeseburgers',
         },
       }
     );
+    console.log('response ', response);
+    return response;
   } catch (err: unknown) {
     if (err instanceof Error) handleError(err);
   }
