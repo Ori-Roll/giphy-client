71f2c69939c5e2181fa0eaebbf106ce73023c678
Ori Roll <oriroll@gmail.com>
2022-08-12 14:26:09 +0300

    added gifAutocompleteBase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { GifAutocomplete } from './components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">GIPHY SEARCH</header>
+      <div>
+        <GifAutocomplete />
+      </div>
     </div>
   );
 }
---
+++ src/components/GifAutocomplete/GifAutocompleteBase.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './gifAutocomplete.module.css';
+
+interface GifAutocompleteBaseProps {
+  value: string;
+  onChange: (value: string) => void;
+  children: any;
+}
+
+export const GifAutocompleteBase: React.FC<GifAutocompleteBaseProps> = ({
+  value,
+  onChange,
+  children,
+}) => {
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <div className={styles['autocomplete-wrapper']}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChangeHandler}
+        className={styles['autocomplete-input']}
+      />
+      <div className={styles['autocomplete-children-wrapper']}>{children}</div>
+    </div>
+  );
+};
