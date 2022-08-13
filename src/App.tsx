a35793ea5a730fe5888e6f84882dc945236576e0
Ori Roll <oriroll@gmail.com>
2022-08-13 21:29:22 +0300

    Router added - Added lib - Added route to lister and - Added Route to ViewPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import { GifAutocomplete } from './components';
+import { GifLister } from './components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ViewPage } from './components/ViewPage/Components/ViewPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">GIPHY SEARCH</header>
-      <div>
-        <GifAutocomplete />
+    <Router>
+      <div className="App">
+        <header className="App-header">GIPHY SEARCH</header>
+        <Routes>
+          <Route path=":gifId" element={<ViewPage />} />
+          <Route path="*" element={<GifLister />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
